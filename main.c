cb21ca30eb5497059a23b37c0a603047c76f6d27
William Oberndorfer <will.oberndorfer@gmail.com>
2017-04-03 01:43:56 -0400

    adding game menu

--- main.c
+++ main.c
@@ -1,12 +1,61 @@
 #include <stdio.h>
+#include <string.h>
+
 #include "egnoramoose.h"
+
+#define COMMAND_BUFSZ 10
+
+void print_usage(void);
 
 int main(int argc, char *argv[])
 {
-    Eg *eg = eg_make(5, 3, 2);
-    eg_print(eg);
-    eg_jump(eg, 1, 0, L_RIGHT);
-    eg_print(eg);
+    int size;
+    int srow = 0;
+    int scol = 0;
+    if (argc == 1) {
+        size = 5;
+    }
+    if (argc > 4) {
+        fprintf(stderr, "Too many arguments\n");
+        print_usage();
+        return -1;
+    }
+    else if (argc > 1) {
+        if (sscanf(argv[1], "%d", &size) != 1) {
+            fprintf(stderr, "Invalid arguments\n");
+            print_usage();
+            return -1;
+        }
+        if (argc == 4) {
+            if (sscanf(argv[2], "%d", &srow) != 1 ||
+                sscanf(argv[3], "%d", &scol) != 1) {
+                fprintf(stderr, "Invalid arguments\n");
+                print_usage();
+                return -1;
+            }
+        }
+    }
+
+    Eg *eg = eg_make(size, srow, scol);
+    char buffer[COMMAND_BUFSZ];
+    int exit_status = 0;
+
+    do {
+        eg_print(eg);
+        printf("\n(J)ump, (N)ew, (Q)uit: ");
+        fgets(buffer, COMMAND_BUFSZ, stdin);
+        if (!strncmp(buffer, "J", COMMAND_BUFSZ) ||
+            !strncmp(buffer, "j", COMMAND_BUFSZ)) {
+            printf("Enter row and column to jump to: ");
+        }
+    } while (exit_status == 0);
+
     eg_destroy(eg);
     return 0;
+}
+
+void print_usage(void)
+{
+    fprintf(stderr, "Eg-no-ra-moose, a Cracker Barrel boredom simulator\n\n"
+            "usage: `egnoramoose [SIZE] [SROW] [SCOL]`\n");
 }
